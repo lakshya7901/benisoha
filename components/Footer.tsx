c7a4df9310e6ff6f80ad032cf768e6553c4f0f63
Lakshya Sharma <lakshya7901@gmail.com>
2026-04-07 23:35:17 +0530

    Social Icons rearranged

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,11 +3,11 @@ export default function Footer() {
 
   const socials = [
     {
-      label: "YouTube",
-      href: "https://youtube.com/@benisohafusion",
+      label: "Facebook",
+      href: "https://facebook.com/benisohafusion",
       icon: (
         <svg viewBox="0 0 24 24" className="w-4 h-4" fill="currentColor">
-          <path d="M23.5 6.2a3 3 0 00-2.1-2.1C19.5 3.5 12 3.5 12 3.5s-7.5 0-9.4.6a3 3 0 00-2.1 2.1C0 8.1 0 12 0 12s0 3.9.5 5.8a3 3 0 002.1 2.1c1.9.6 9.4.6 9.4.6s7.5 0 9.4-.6a3 3 0 002.1-2.1C24 15.9 24 12 24 12s0-3.9-.5-5.8zm-13.9 9.4V8.4l6.3 3.6-6.3 3.6z" />
+          <path d="M24 12.07C24 5.41 18.63 0 12 0S0 5.41 0 12.07C0 18.1 4.39 23.1 10.13 24v-8.44H7.08v-3.49h3.04V9.41c0-3.02 1.8-4.7 4.54-4.7 1.31 0 2.68.24 2.68.24v2.97h-1.51c-1.49 0-1.95.93-1.95 1.88v2.27h3.32l-.53 3.49h-2.79V24C19.61 23.1 24 18.1 24 12.07z" />
         </svg>
       ),
     },
@@ -39,14 +39,14 @@ export default function Footer() {
       ),
     },
     {
-      label: "Facebook",
-      href: "https://facebook.com/benisohafusion",
+      label: "YouTube",
+      href: "https://youtube.com/@benisohafusion",
       icon: (
         <svg viewBox="0 0 24 24" className="w-4 h-4" fill="currentColor">
-          <path d="M24 12.07C24 5.41 18.63 0 12 0S0 5.41 0 12.07C0 18.1 4.39 23.1 10.13 24v-8.44H7.08v-3.49h3.04V9.41c0-3.02 1.8-4.7 4.54-4.7 1.31 0 2.68.24 2.68.24v2.97h-1.51c-1.49 0-1.95.93-1.95 1.88v2.27h3.32l-.53 3.49h-2.79V24C19.61 23.1 24 18.1 24 12.07z" />
+          <path d="M23.5 6.2a3 3 0 00-2.1-2.1C19.5 3.5 12 3.5 12 3.5s-7.5 0-9.4.6a3 3 0 00-2.1 2.1C0 8.1 0 12 0 12s0 3.9.5 5.8a3 3 0 002.1 2.1c1.9.6 9.4.6 9.4.6s7.5 0 9.4-.6a3 3 0 002.1-2.1C24 15.9 24 12 24 12s0-3.9-.5-5.8zm-13.9 9.4V8.4l6.3 3.6-6.3 3.6z" />
         </svg>
       ),
-    },
+    }
   ];
   
   const renderFollowUs = () => (
